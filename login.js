0affbab62cc3cf5faefeee6618c535888de37c7a
Jasveer-Shah <talkjasveer@gmail.com>
2022-05-01 12:31:51 -0400

    add

--- login.js
+++ login.js
@@ -5,39 +5,39 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const existingEmail = urlParams.get('existingEmail');
 const registered = urlParams.get('registered');
-if(existingEmail){
+if (existingEmail) {
     loginForm.email.value = existingEmail
 }
-if(registered){
+if (registered) {
     document.querySelector('.registered-alert').style.display = "block";
 }
 
-loginForm.addEventListener("submit", (e)=>{
+loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log(loginForm);
     let payload = {
-         email: loginForm.email.value,
-         password: loginForm.password.value
+        email: loginForm.email.value,
+        password: loginForm.password.value
     }
-    fetch(apiUrl+ "/login", {
+    fetch(apiUrl + "/login", {
         method: "post",
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(payload)
     })
-    .then((response)=>{
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error("something went wrong");
-        }
-    }) //fetch returns a promise already
-    .then((response)=>{
-        localStorage.setItem('token', response.token)
-        location.href = "/";
-        console.log("response");
-    })
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("something went wrong");
+            }
+        }) //fetch returns a promise already
+        .then((response) => {
+            localStorage.setItem('token', response.token)
+            location.href = "/";
+            console.log(response);
+        })
 })
 
 
